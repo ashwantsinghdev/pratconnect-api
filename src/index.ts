50b13bee130cc08f1977c1d335e1e562e7c8de4a
Ashwant Singh <ashwantconset@gmail.com>
2026-07-04 16:48:29 +0530

    http server with socket.io friend router and status socket

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,16 @@ import AuthRouter from "./router/auth.router"
 import corsConfig from "./utils/cors"
 import AuthMiddleware from "./middleware/auth.middleware"
 import StorageRouter from "./router/storage.router"
+import FriendRouter from "./router/friend.router"
+import StatusSocket from "./socket/status.socket"
+import { Server } from "socket.io"
+
 
 const app = express();
 const server = createServer(app);
 server.listen(process.env.PORT || 8080);
+const io=new Server(server,{cors:corsConfig})
+
 
 app.use(cors(corsConfig));
 app.use(cookieParser());
@@ -24,6 +30,11 @@ app.use(express.urlencoded({ extended: false }));
 
 
 
+
 app.use("/auth",AuthRouter)
 app.use("/storage",AuthMiddleware,StorageRouter)
+app.use("/friend", AuthMiddleware, FriendRouter);
+
+
+StatusSocket(io)
 
